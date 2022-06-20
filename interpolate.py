c4cef3d7983934f0807d1cf2093a22259d2089fe
simeonreusch <simeon.reusch@desy.de>
2022-06-20 17:31:04 +0200

    now it plots

--- interpolate.py
+++ interpolate.py
@@ -1,4 +1,4 @@
-import os
+import os, random
 import numpy as np
 import pandas as pd
 
@@ -11,7 +11,13 @@ datadir = "data"
 lcfile = os.path.join(datadir, "plasticc_train_lightcurves.csv.gz")
 lcs = pd.read_csv(lcfile)
 
-category_mapping = {
+metafile = os.path.join(datadir, "plasticc_train_metadata.csv.gz")
+META = pd.read_csv(metafile)
+
+
+BAND_COLORS = ["C4", "C2", "C3", "C1", "k", "C5"]
+BAND_NAMES = ["u", "g", "r", "i", "z", "y"]
+CATEGORY_MAPPING = {
     90: "SN1a",
     67: "SN1a-91bg",
     52: "SN1ax",
@@ -28,62 +34,95 @@ category_mapping = {
     6: "Microlens",
 }
 
-test_lc = lcs[lcs["object_id"] == 54338231]
 
-band_colors = ["C4", "C2", "C3", "C1", "k", "C5"]
-band_names = ["u", "g", "r", "i", "z", "y"]
+ids = lcs["object_id"].unique()
 
-fig, ax = plt.subplots()
 
-for band, group in test_lc.groupby("passband"):
+def interpolate_lc(object_id, plot=False):
 
-    k = ConstantKernel(constant_value=1.0, constant_value_bounds=(1e-3, 1e3)) * RBF(
-        length_scale=1, length_scale_bounds=(1e-3, 1e3)
-    )
+    lc = lcs[lcs["object_id"] == object_id]
 
-    x = np.atleast_2d(group["mjd"].values).T
-    y = group["flux"].values
-    y_err = group["flux_err"].values
+    t_id = META[META.object_id == object_id]["target"].values[0]
 
-    gp = GaussianProcessRegressor(kernel=k, alpha=y_err**2, n_restarts_optimizer=1000)
+    classification = CATEGORY_MAPPING[t_id]
 
-    gp.fit(x, y)
+    if plot:
+        fig, ax = plt.subplots(figsize=(5, 4))
 
-    start_mod = 59580.0343
-    end_mod = 60674.363
+    for band, group in lc.groupby("passband"):
 
-    n_points = int(end_mod - start_mod)
+        k = ConstantKernel(constant_value=1.0, constant_value_bounds=(1e-3, 1e3)) * RBF(
+            length_scale=1, length_scale_bounds=(1e-3, 1e3)
+        )
 
-    x_sampled = np.atleast_2d(np.linspace(start_mod, end_mod, n_points)).T
+        x = np.atleast_2d(group["mjd"].values).T
+        y = group["flux"].values
+        y_err = group["flux_err"].values
 
-    y_sampled_g, sigma_sampled_g = gp.predict(x_sampled, return_std=True)
+        gp = GaussianProcessRegressor(
+            kernel=k, alpha=y_err**2, n_restarts_optimizer=1000
+        )
 
-    # Make the prediction on the meshed x-axis (ask for MSE as well)
-    # x_pred = np.atleast_2d(np.linspace(start_mod, end_mod, n_points)).T
-    y_pred_g, sigma_g = gp.predict(x_sampled, return_std=True)
+        gp.fit(x, y)
 
-    ax.plot(x_sampled, y_pred_g, band_colors[band] + "-", label="Prediction", alpha=0.2)
-    ax.fill(
-        np.concatenate([x_sampled, x_sampled[::-1]]),
-        np.concatenate(
-            [y_pred_g - 1.9600 * sigma_g, (y_pred_g + 1.9600 * sigma_g)[::-1]]
-        ),
-        alpha=0.1,
-        fc=band_colors[band],
-        ec="None",
-    )  # , label='95% confidence interval')
+        start_mod = 59580.0343
+        end_mod = 60674.363
 
-    ax.errorbar(
-        x=group["mjd"],
-        y=group["flux"],
-        yerr=group["flux_err"],
-        fmt="o",
-        color=band_colors[band],
-        label=band_names[band],
-    )
+        n_points = int(end_mod - start_mod)
 
-plt.legend(ncol=6)
-ax.set_ylabel("flux")
-ax.set_xlabel("mjd")
-plt.savefig("test.png")
-plt.close()
+        x_sampled = np.atleast_2d(np.linspace(start_mod, end_mod, n_points)).T
+
+        y_sampled_g, sigma_sampled_g = gp.predict(x_sampled, return_std=True)
+
+        # Make the prediction on the meshed x-axis (ask for MSE as well)
+        # x_pred = np.atleast_2d(np.linspace(start_mod, end_mod, n_points)).T
+        y_pred_g, sigma_g = gp.predict(x_sampled, return_std=True)
+
+        if plot:
+
+            ax.plot(
+                x_sampled,
+                y_pred_g,
+                BAND_COLORS[band] + "-",
+                label="Prediction",
+                alpha=0.2,
+            )
+
+            ax.fill(
+                np.concatenate([x_sampled, x_sampled[::-1]]),
+                np.concatenate(
+                    [y_pred_g - 1.9600 * sigma_g, (y_pred_g + 1.9600 * sigma_g)[::-1]]
+                ),
+                alpha=0.1,
+                fc=BAND_COLORS[band],
+                ec="None",
+            )  # , label='95% confidence interval')
+
+            ax.errorbar(
+                x=group["mjd"],
+                y=group["flux"],
+                yerr=group["flux_err"],
+                fmt="o",
+                color=BAND_COLORS[band],
+                label=BAND_NAMES[band],
+            )
+
+    if plot:
+        ax.set_title(f"ID = {object_id} / Type = {classification}")
+        plt.legend(ncol=6)
+        ax.set_ylabel("flux")
+        ax.set_xlabel("mjd")
+
+        plotdir = "plots"
+        if not os.path.exists(plotdir):
+            os.makedirs(plotdir)
+        plt.savefig(os.path.join(plotdir, f"{object_id}.png"), dpi=300)
+        plt.close()
+
+    return None
+
+
+random_id = random.choice(ids)
+
+interpolate_lc(object_id=random_id, plot=True)
+print("Regression done")
