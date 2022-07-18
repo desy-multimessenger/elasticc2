19459d55f7ff57a336902a9747b12060563874ad
simeonreusch <simeon.reusch@desy.de>
2022-07-18 10:51:25 +0200

    added trained models

--- elasticc/run.py
+++ elasticc/run.py
@@ -12,25 +12,15 @@ logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+for i, stage in enumerate(["2a", "2b"]):
 
-m = Model(
-    stage="1",
-    path_to_trainingset=path_to_trainingset,
-    n_iter=100,
-    random_state=60,
-    grid_search_sample_size=20000,
-)
-m.split_sample()
-
-t_start = time.time()
-
-m.train()
-
-t_end = time.time()
-
-logger.info("------------------------------------")
-logger.info("           FITTING DONE             ")
-logger.info(f"  This took {t_end-t_start} seconds")
-logger.info("------------------------------------")
-
-m.evaluate()
+    m = Model(
+        stage=stage,
+        path_to_trainingset=path_to_trainingset,
+        n_iter=200,
+        random_state=65 + i,
+        grid_search_sample_size=10000,
+    )
+    m.split_sample()
+    m.train()
+    m.evaluate()
