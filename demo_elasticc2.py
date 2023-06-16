729a757065451f809d6516541d284797c836899e
simeonreusch <simeon.reusch@desy.de>
2023-06-16 16:09:10 +0200

    update file

--- demo_elasticc2.py
+++ demo_elasticc2.py
@@ -2,6 +2,7 @@ import logging
 import os
 from pathlib import Path
 
+from elasticc2.taxonomy import root
 from elasticc2.trainmodel_elasticc2 import XgbModel
 from elasticc2.utils import load_config
 
@@ -15,61 +16,22 @@ logger.setLevel(logging.INFO)
 basedir = os.environ.get("ELASTICCDATA")
 if basedir is None:
     raise ValueError("Please set an environment-variable for 'ELASTICCDATA'")
-# path_to_featurefiles = Path(basedir) / "feature_extraction" / "trainset_all_max3"
-path_to_featurefiles = Path(basedir) / "feature_extraction" / "trainset_early_max10"
-
+path_to_featurefiles = Path(basedir) / "feature_extraction" / "trainset_all_max3"
 
 config = load_config()
-cl_inv = config["classes_inv"]
-# Train classifer to distinguish SNe Ia (2222) from SNIbc and SNII (2223,2224)
-# Using a subset of max 10000 rows from the feature filws
-# pos_tax = [cl_inv["snia"]]
-# neg_tax = [cl_inv["snibc"], cl_inv["snibc"]]
-# max_taxlength = 10000
 
-# Train classifer to distinguish AGN from periodic stars
-# Using a subset of max 10000 rows from the feature files
-# pos_tax = [cl_inv["agn"]]
-# neg_tax = [cl_inv["agn"], cl_inv["rrlyrae"], cl_inv["deltascuti"], cl_inv["eb"]]
-# max_taxlength = 10000
+cl_inv = config["classes_inv"]["all"]
 
-# # Train classifier to distinguish KN from ... everything else
-pos_tax = [cl_inv["kn"]]
-cl_all = cl_inv["all"]
-cl_all.remove(cl_inv["kn"])
-neg_tax = cl_all
-
-
-# max_taxlength = 10000
-# To use files where alerts with more than 10 det have been removed uncomment below
-# path_to_featurefiles = '/home/jnordin/data/elasticc2/trainset_early_max10'
-
-# Train classifier to separate recurrent from non-recurrent alerts using all alerts
-# pos_tax = [
-#     2221,
-#     2222,
-#     2223,
-#     2224,
-#     2225,
-#     2226,
-#     2231,
-#     2232,
-#     2233,
-#     2234,
-#     2235,
-#     2241,
-#     2242,
-#     2243,
-#     2244,
-#     2245,
-#     2246,
-# ]
-# neg_tax = [2321, 2322, 2323, 2324, 2325, 2326, 2331, 2332]
+pos_tax = config["galactic"]
+neg_tax = [i for i in cl_inv if i not in pos_tax]
 max_taxlength = 10000
+
 
 model = XgbModel(
     pos_tax=pos_tax,
     neg_tax=neg_tax,
+    pos_name="galactic",
+    neg_name="non_galactic",
     path_to_featurefiles=path_to_featurefiles,
     max_taxlength=max_taxlength,
 )
