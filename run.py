589ce6b530fe2362ed2f2dab74431b3ffb88bd5c
simeonreusch <simeon.reusch@desy.de>
2023-07-03 08:14:15 +0200

    bugfix snlong grid sample

--- run.py
+++ run.py
@@ -26,71 +26,6 @@ if basedir is None:
     raise ValueError("Please set an environment-variable for 'ELASTICCDATA'")
 path_to_featurefiles = Path(basedir) / "feature_extraction" / "trainset"
 
-setups_binary = {
-    1: {
-        "key": "galactic",
-        "pos_tax": config["galactic"],
-        "neg_tax": tax.get_ids(exclude=config["galactic"]),
-        "neg_name": "non_galactic",
-    },
-    2: {
-        "key": "recurring",
-        "pos_tax": tax.rec.get_ids(),
-        "neg_tax": tax.get_ids(exclude=tax.rec.get_ids()),
-        "neg_name": "non_recurring",
-    },
-    3: {
-        "key": "kn",
-        "pos_tax": tax.ids_from_keys("kn"),
-        "neg_tax": tax.get_ids(exclude=tax.ids_from_keys("kn")),
-        "neg_name": "all_other",
-    },
-    4: {
-        "key": "parsnip",
-        "pos_tax": [*tax.nrec.sn.get_ids(), *tax.nrec.long.get_ids()],
-        "neg_tax": tax.get_ids(
-            exclude=[*tax.nrec.sn.get_ids(), *tax.nrec.long.get_ids()]
-        ),
-        "neg_name": "all_other",
-    },
-    5: {
-        "key": "ceph",
-        "pos_tax": [tax.rec.periodic.ceph.id],
-        "neg_tax": tax.rec.periodic.get_ids(exclude=[tax.rec.periodic.ceph.id]),
-        "neg_name": "other_stars",
-    },
-    6: {
-        "key": "deltasc",
-        "pos_tax": [tax.rec.periodic.deltasc.id],
-        "neg_tax": tax.rec.periodic.get_ids(exclude=[tax.rec.periodic.deltasc.id]),
-        "neg_name": "other_stars",
-    },
-    7: {
-        "key": "eb",
-        "pos_tax": [tax.rec.periodic.eb.id],
-        "neg_tax": tax.rec.periodic.get_ids(exclude=[tax.rec.periodic.eb.id]),
-        "neg_name": "other_stars",
-    },
-    8: {
-        "key": "rrlyrae",
-        "pos_tax": [tax.rec.periodic.rrlyrae.id],
-        "neg_tax": tax.rec.periodic.get_ids(exclude=[tax.rec.periodic.rrlyrae.id]),
-        "neg_name": "other_stars",
-    },
-    9: {
-        "key": "ulens+mdwarf",
-        "pos_tax": tax.ids_from_keys(["ulens", "mdwarf"]),
-        "neg_tax": tax.rec.periodic.get_ids(),
-        "neg_name": "periodic_star",
-    },
-    10: {
-        "key": "kn",
-        "pos_tax": tax.ids_from_keys("kn"),
-        "neg_tax": [*tax.nrec.sn.get_ids(), *tax.nrec.long.get_ids()],
-        "neg_name": "parsnip",
-    },
-}
-
 setups_binary_production = {
     1: {
         "key": "galactic",
@@ -172,10 +107,7 @@ def run_setup_binary(num: int):
 
     from elasticc2.train_binary_model import XgbModel
 
-    if neg_name == "long":
-        grid_search_sample_size = 1000000
-    else:
-        grid_search_sample_size = 10000
+    grid_search_sample_size = 10000
 
     model = XgbModel(
         pos_tax=pos_tax,
@@ -216,7 +148,7 @@ def run_setup_multivar(num: int):
     model.evaluate()
 
 
-for setup in [1, 2, 3, 4, 5, 6]:
+for setup in [6]:
     max_taxlength = -1
     run_setup_binary(setup)
 
